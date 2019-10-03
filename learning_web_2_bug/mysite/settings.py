8b8afb7985d26b2b6cce1c871fda7fa3029192a2
xbb <1927552890@qq.com>
2019-10-03 16:07:02 +0800

    database_re

--- learning_web_2_bug/mysite/settings.py
+++ learning_web_2_bug/mysite/settings.py
@@ -86,10 +86,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysite',
+        'USER': 'root',
+        'password': 'bwt4850694',
+        'HOST':'152.136.109.221',
+        'PORT': '3306',
     }
 }
 
